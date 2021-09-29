b3576f0316ba3feaeef22839a12252266c4359c2
leanwayne <lean_rap50@hotmail.com>
2021-09-29 13:39:26 -0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <spam className= 'header'>Movies App</spam>
+        </div>
+    )
+}
+export default Header
